2fc61274c6ae9610d6bd5e6f80399af1bd92818a
Everett Gaius Vergara <evergara@shinra.com.ph>
2023-02-20 22:56:59 +0800

    added procedures

---
+++ Regular Project/build/CMakeFiles/REGULAR.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/REGULAR.dir/main.cpp.obj"
+  "CMakeFiles/REGULAR.dir/main.cpp.obj.d"
+  "REGULAR.exe"
+  "REGULAR.exe.manifest"
+  "REGULAR.pdb"
+  "libREGULAR.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/REGULAR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Regular Project/build/CMakeFiles/REGULAR.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for REGULAR.
+# This may be replaced when dependencies are built.
